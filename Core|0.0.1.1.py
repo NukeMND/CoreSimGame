c819ef48d3e1620877c4c9ab295936513dde0436
NukeMND <77718850+NukeMND@users.noreply.github.com>
2022-06-15 20:56:20 +0300

    Core 0.0.1

--- Core|0.0.1.1.py
+++ Core|0.0.1.1.py
@@ -10,26 +10,33 @@ from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 #Importing BoxLayout, its simplier for me. So its good for this app:)
 from kivy.uix.boxlayout import BoxLayout
+#Clock for updating screen every second
+from kivy.clock import Clock
 #Making Main Class, you can redact only in it
 class MyApp(App):
 	#rods counter
 	rods=0
+	#temperature counter
+	temperature=0
 	#Building App
 	def build(self):
 		#Layouts
 		BL=BoxLayout(orientation = "vertical",
 		padding=[40])
 		#GUI interface
-		#Label making...
-		self.RodsLabel=Label(text="Rods are:")
+		#Rods Label
+		self.RodsLabel=Label(text="Rods:")
 		BL.add_widget(self.RodsLabel)
+		#Temperature Label
+		self.temperatureLabel=Label(text="Temperature:")
+		BL.add_widget(self.temperatureLabel)
 		#First Button
 		BL.add_widget(
 		Button( text ="Rods +1%",
 		on_release = self.rods_up_press, ))
 		#Second Button
 		BL.add_widget(
-		Button( text ="Rods -1%",
+		Button( text ="Rods -1%", 
 		on_release = self.rods_down_press, ))
 		#Returning BoxLayout
 		return BL
@@ -42,12 +49,15 @@ class MyApp(App):
 			MyApp.rods+=1
 		#changing text of label
 		self.RodsLabel.text="Rods: "+str(MyApp.rods)+"%"
+		Clock.schedule_interval(self.update, 2)
 	#On Down press Function
 	def rods_down_press(self, instance):
 		if MyApp.rods>0:
 			MyApp.rods-=1
 		self.RodsLabel.text="Rods: "+str(MyApp.rods)+"%"
-	pass
+	def update(self, dt):
+		self.temperature=round(self.temperature+(self.rods)/15)
+		self.temperatureLabel.text="Temperature:"+str(self.temperature)+"C"
 if __name__ == "__main__":
 	#And Finally run
 	MyApp().run()
